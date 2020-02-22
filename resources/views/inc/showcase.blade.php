5b9caa042f63c7e780ff4dc50e0c76aa3b2006bf
kmagdi <katay.magdi@gmail.com>
2020-02-22 23:19:48 +0100

    commit 1

---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,7 @@
+<div class="jumbotron text-center text-white" style="background-image: url('img/2.jpg');">
+  <div class="container">
+    <h1>Üdvözlünk az oldalunkon</h1>
+      <p class="lead">Üdvözlünk az első Laravel oldalunkon...minden kezdet nehéz</p>
+      <!--<img src="{{ URL::to('/img/2.jpg') }}">-->
+  </div>
+</div>
